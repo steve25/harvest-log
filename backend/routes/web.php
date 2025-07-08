cd88e54df08d485e2771cfc249486f8fda45bf1c
steve25 <stefan.pocsik@gmail.com>
2025-07-08 17:30:17 +0200

    Create get fields endpoint

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Models\Field;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/fields', function () {
+    return response()->json([
+        'fields' => Field::all()
+    ]);
 });
