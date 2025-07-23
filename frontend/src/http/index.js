9ead6fba3b1d5c113a8fae9f8704189ac4f65121
steve25 <stefan.pocsik@gmail.com>
2025-07-23 21:40:15 +0200

    trying simple prod mode

--- frontend/src/http/index.js
+++ frontend/src/http/index.js
@@ -2,7 +2,7 @@ import { getCookie } from '@/utils'
 import axios from 'axios'
 
 const http = axios.create({
-  baseURL: 'http://localhost:8000/',
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 1000,
   headers: {
     'Content-Type': 'application/json',
